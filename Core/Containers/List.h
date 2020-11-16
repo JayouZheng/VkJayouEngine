81507306501427f846ee698657b9f62a38f30b1f
Jayou <906832263@qq.com>
2020-11-16 15:27:59 +0800

    add [Natural] & List.

---
+++ Core/Containers/List.h
@@ -0,0 +1,72 @@
+﻿//
+// List.h
+//
+
+#pragma once
+
+#include "Core/TypeDef.h"
+#include "Core/Math/Sequence.h"
+
+template<typename T>
+class List
+{
+public:
+
+    List() : 
+        m_data(nullptr) {}
+
+    List(uint32 InSize) : 
+        m_data(nullptr)
+    {
+        Resize(InSize);
+    }
+
+public:
+
+    uint32 Length() const
+    {
+        return m_ids.Length();
+    }
+
+    void Clear()
+    {
+        delete[] m_data;
+        m_data = nullptr;
+    }
+
+    void Resize(uint32 InSize)
+    {
+        if (m_data != nullptr)
+            Clear();
+
+        m_data = new T[InSize];
+        m_ids.Resize(InSize);
+    }
+
+public:
+
+    ~List()
+    {
+        Clear();
+    }
+
+    T& operator[](uint32 m_ids)
+    {
+        return m_data[m_ids];
+    }
+
+    T* begin()
+    {
+        return m_data;
+    }
+
+    T* end()
+    {
+        return (m_data + Length());
+    }
+
+protected:
+
+    T* m_data;
+    Math::Sequence::Natural m_ids;
+};
---
+++ Core/Math/Sequence.h
@@ -0,0 +1,76 @@
+﻿//
+// Sequence.h
+//
+
+#pragma once
+
+#include "Core/BaseType.h"
+
+namespace Math
+{
+    namespace Sequence
+    {
+        class Natural
+        {
+        public:
+
+            Natural() : 
+                m_length(0u),
+                m_sequence(nullptr) {}
+
+            Natural(uint32 InSize) :
+                m_length(0u),
+                m_sequence(nullptr) 
+            {
+                Resize(InSize);
+            }
+
+        public:
+
+            uint32 Length() const
+            {
+                return m_length;
+            }
+
+            void Clear()
+            {
+                delete[] m_sequence;
+                m_sequence = nullptr;
+            }
+
+            void Resize(uint32 InSize)
+            {
+                m_length = InSize;
+
+                if (m_sequence != nullptr)
+                    Clear();
+
+                m_sequence = new int[m_length];
+                for (int i = 0; i < m_length; i++)
+                    m_sequence[i] = i;
+            }
+
+        public:
+
+            ~Natural()
+            {
+                Clear();
+            }
+
+            const uint32* begin()
+            {
+                return m_sequence;
+            }
+
+            const uint32* end()
+            {
+                return (m_sequence + m_length);
+            }
+
+        private:
+
+            uint32  m_length;
+            uint32* m_sequence;
+        };
+    }
+}
