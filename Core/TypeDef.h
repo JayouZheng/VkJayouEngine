b3fbcdb639c9513a52c28552732b0b1ef276ea7f
Jayou <906832263@qq.com>
2020-11-16 15:24:48 +0800

    move var type def to BaseType.h

--- Core/TypeDef.h
+++ Core/TypeDef.h
@@ -81,25 +81,7 @@
 #define _numeric_max(type) std::numeric_limits<type>::max()
 
 
-#include <cstdint>
-
-using int8  = std::int8_t;
-using int16 = std::int16_t;
-using int32 = std::int32_t;
-using int64 = std::int64_t;
-
-using uint8  = std::uint8_t;
-using uint16 = std::uint16_t;
-using uint32 = std::uint32_t;
-using uint64 = std::uint64_t;
-
-using u16char = char16_t;
-using u32char = char32_t;
-
-using byte = uint8;
-
-using iBool = int32;
-using uBool = uint32;
+#include "BaseType.h"
 
 // Function Ptr Table.
 typedef void(__cdecl *PFVOID)    (void);
