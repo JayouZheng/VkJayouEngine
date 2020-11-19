2222ebdf8097ad1dcd521154888965dfa905f490
Jayou <906832263@qq.com>
2020-11-19 16:20:56 +0800

    Update ModuleLoader.h

--- Core/Utility/ModuleLoader.h
+++ Core/Utility/ModuleLoader.h
@@ -17,28 +17,7 @@ public:
 	void Free();
 
 	template<typename T>
-	T GetInterface(const std::string& InInterfaceName)
-	{
-		if (m_pModule != nullptr)
-		{
-#if PLATFORM_WINDOW	
-			T api = nullptr;
-			api = (T)GetProcAddress((HMODULE)m_pModule, InInterfaceName.c_str());
-
-			if (api == nullptr)
-			{
-				_log_error("Fail to find API [" + InInterfaceName + "]!", LogSystem::Category::ModuleLoader);
-				return nullptr;
-			}
-			return api;
-#endif
-		}
-		else
-		{
-			_log_error("Fail to find API [" + InInterfaceName + "], the module [" + m_moduleName + "] was not load!", LogSystem::Category::ModuleLoader);
-			return nullptr;
-		}
-	}
+	T GetInterface(const std::string& InInterfaceName);
 
 private:
 
@@ -48,3 +27,29 @@ private:
 	iBool       m_bResult;
 	std::string m_moduleName;	
 };
+
+// #include "ModuleLoader.inl"
+
+template<typename T>
+T ModuleLoader::GetInterface(const std::string& InInterfaceName)
+{
+	if (m_pModule != nullptr)
+	{
+#if PLATFORM_WINDOW	
+		T api = nullptr;
+		api = (T)GetProcAddress((HMODULE)m_pModule, InInterfaceName.c_str());
+
+		if (api == nullptr)
+		{
+			_log_error("Fail to find API [" + InInterfaceName + "]!", LogSystem::Category::ModuleLoader);
+			return nullptr;
+		}
+		return api;
+#endif
+	}
+	else
+	{
+		_log_error("Fail to find API [" + InInterfaceName + "], the module [" + m_moduleName + "] was not load!", LogSystem::Category::ModuleLoader);
+		return nullptr;
+	}
+}
